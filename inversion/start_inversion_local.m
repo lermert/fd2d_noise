e3873a276614868b48b19de5a7680a91b66cb5e7
sagerk <korbinian.sager@erdw.ethz.ch>
2015-06-17 00:48:22 +0200

    test new misfit function

--- inversion/start_inversion_local.m
+++ inversion/start_inversion_local.m
@@ -5,7 +5,7 @@ path(path,genpath('../'))
 x0 = reshape( zeros(300,300),[],1 );
 
 % run inversion
-x = 4.8e10 * (1+LBFGS(x0,'get_obj_grad',0.2,5));
+x = 4.8e10 * ( 1 + LBFGS(x0,'get_obj_grad',0.2,5) );
 
 % save solution
 save ../output/solution.mat x
