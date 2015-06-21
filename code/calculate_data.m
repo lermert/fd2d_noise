c4523d0e7d6888ca0de8f478c759a4d6eba50850
sagerk <korbinian.sager@erdw.ethz.ch>
2015-06-21 19:06:06 +0200

    parallel data calculation

--- code/calculate_data.m
+++ code/calculate_data.m
@@ -2,16 +2,12 @@
 clear all
 close all
 
-%%%%%%%%%%%%%%%%%%%%%%%%%%%
-% NEED A PARALLEL VERSION %
-%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
+mode = 'slow';
+% mode = 'fast';
 
 tic
 
-addpath(genpath('../'))
-flip_sr = 'no';
-
+% define model
 [Lx,Lz,nx,nz,dt,nt,order,model_type] = input_parameters();
 [X,Z,x,z,dx,dz] = define_computational_domain(Lx,Lz,nx,nz);
 [mu,rho] = define_material_parameters(nx,nz,model_type); 
@@ -20,27 +16,27 @@ output_specs
 
 
 % define receiver array
-nr_x = 4;
-nr_z = 4;
-array = zeros(nr_x*nr_z,2);
-for i = 1:nr_x
-    for j = 1:nr_z
-        % array( (i-1)*nr_x + j, 1 ) = 4*width + ( i-1 )*(Lx-8*width)/(nr_x-1);
-        % array( (i-1)*nr_z + j, 2 ) = 4*width + ( j-1 )*(Lz-8*width)/(nr_z-1);
-        
-        array( (i-1)*nr_x + j, 1 ) = 0.9e6 + ( i-1 )*0.25e6;
-        array( (i-1)*nr_z + j, 2 ) = 0.6e6 + ( j-1 )*0.25e6;
-    end
-end
-
-% nr_x = 2;
-% nr_z = 1;
+% nr_x = 4;
+% nr_z = 4;
 % array = zeros(nr_x*nr_z,2);
 % for i = 1:nr_x
-%         % array( i, 1 ) = Lx/2 + (-1)^i * 0.15*Lx;
-%         array( i, 1 ) = Lx/2 + (-1)^i * 0.08*Lx - Lx/5;
-%         array( i, 2 ) = Lz/2;
+%     for j = 1:nr_z
+%         % array( (i-1)*nr_x + j, 1 ) = 4*width + ( i-1 )*(Lx-8*width)/(nr_x-1);
+%         % array( (i-1)*nr_z + j, 2 ) = 4*width + ( j-1 )*(Lz-8*width)/(nr_z-1);
+%         
+%         array( (i-1)*nr_x + j, 1 ) = 0.9e6 + ( i-1 )*0.25e6;
+%         array( (i-1)*nr_z + j, 2 ) = 0.6e6 + ( j-1 )*0.25e6;
+%     end
 % end
+
+nr_x = 2;
+nr_z = 1;
+array = zeros(nr_x*nr_z,2);
+for i = 1:nr_x
+        array( i, 1 ) = Lx/2 + (-1)^i * 0.15*Lx;
+        % array( i, 1 ) = Lx/2 + (-1)^i * 0.08*Lx - Lx/5;
+        array( i, 2 ) = Lz/2;
+end
 
 
 
@@ -62,24 +58,41 @@ if( strcmp(make_plots,'yes') )
     plot_model
 end
 
-return
+
 % calculate correlations
-nr = size(array,1)-1;
+n_ref = size(ref_stat,1);
+n_rec = size(array,1)-1;
+t = -(nt-1)*dt:dt:(nt-1)*dt;
+c_it = zeros(n_ref,n_rec,length(t));
+
 fprintf('\n')
-for i = 1:size(ref_stat,1)
+flip_sr = 'no';
+for i = 1:n_ref
     
-    if( strcmp(verbose,'yes') )
+    % if( strcmp(verbose,'yes') )
         fprintf('reference station: %i\n',i)
-    end
+    % end
     
     % each reference station will act as a source once
     src = ref_stat(i,:);
     rec = array( find(~ismember(array,src,'rows') ) , :);
     
     % calculate the correlation for each pair
-    [~,~] = run_forward('forward_green',src,rec,i,flip_sr);
-    [c_data( (i-1)*nr + 1 : i*nr , :),t] = run_forward('correlation',src,rec,i,flip_sr);
+    if( strcmp(mode,'slow') )
+%         [~,~] = run_forward('forward_green',src,rec,i,flip_sr);
+        [c_it(i,:,:),~] = run_forward('correlation',src,rec,i,flip_sr);
+    elseif(strcmp(mode,'fast'))
+        [G_2] = run_forward_green_fast_mex(mu, src);
+        [c_it(i,:,:)] = run_forward_correlation_fast_mex(G_2, source_dist, mu, rec);
+    end
     
+end
+
+
+% reorganize correlation vector
+c_data = zeros(n_ref*n_rec,length(t));
+for i = 1:n_ref
+    c_data(i*n_rec,:) = c_it(i,:,:);
 end
 
 
@@ -92,11 +105,8 @@ end
 
 
 % save array and data for inversion
-save(sprintf('../output/interferometry/array_%i_ref_big_test1.mat',size(ref_stat,1)),'array','ref_stat')
-save(sprintf('../output/interferometry/data_%i_ref_big_test1.mat',size(ref_stat,1)),'c_data','t')
+save( sprintf('../output/interferometry/array_%i_ref_big_test1.mat',n_ref), 'array', 'ref_stat')
+save( sprintf('../output/interferometry/data_%i_ref_big_test1.mat',n_ref), 'c_data', 't')
 
-
-% clean up
-rmpath(genpath('../'))
 toc
 
