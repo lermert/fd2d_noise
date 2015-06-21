4bf5f262fa282651159fd66bc6b720b7add9f8ba
sagerk <korbinian.sager@erdw.ethz.ch>
2015-06-21 20:02:17 +0200

    add possibility to calculate data on cluster

--- code/calculate_data.m
+++ code/calculate_data.m
@@ -2,41 +2,44 @@
 clear all
 close all
 
-mode = 'slow';
-% mode = 'fast';
 
-tic
+% mode = 'local';
+mode = 'cluster';
+
 
 % define model
+addpath(genpath('../'))
 [Lx,Lz,nx,nz,dt,nt,order,model_type] = input_parameters();
 [X,Z,x,z,dx,dz] = define_computational_domain(Lx,Lz,nx,nz);
 [mu,rho] = define_material_parameters(nx,nz,model_type); 
 [width] = absorb_specs();
 output_specs
-
+if(strcmp(mode,'cluster'))
+    make_plots = 'no';
+end
 
 % define receiver array
-% nr_x = 4;
-% nr_z = 4;
-% array = zeros(nr_x*nr_z,2);
-% for i = 1:nr_x
-%     for j = 1:nr_z
-%         % array( (i-1)*nr_x + j, 1 ) = 4*width + ( i-1 )*(Lx-8*width)/(nr_x-1);
-%         % array( (i-1)*nr_z + j, 2 ) = 4*width + ( j-1 )*(Lz-8*width)/(nr_z-1);
-%         
-%         array( (i-1)*nr_x + j, 1 ) = 0.9e6 + ( i-1 )*0.25e6;
-%         array( (i-1)*nr_z + j, 2 ) = 0.6e6 + ( j-1 )*0.25e6;
-%     end
-% end
-
-nr_x = 2;
-nr_z = 1;
+nr_x = 4;
+nr_z = 4;
 array = zeros(nr_x*nr_z,2);
 for i = 1:nr_x
-        array( i, 1 ) = Lx/2 + (-1)^i * 0.15*Lx;
-        % array( i, 1 ) = Lx/2 + (-1)^i * 0.08*Lx - Lx/5;
-        array( i, 2 ) = Lz/2;
+    for j = 1:nr_z
+        % array( (i-1)*nr_x + j, 1 ) = 4*width + ( i-1 )*(Lx-8*width)/(nr_x-1);
+        % array( (i-1)*nr_z + j, 2 ) = 4*width + ( j-1 )*(Lz-8*width)/(nr_z-1);
+        
+        array( (i-1)*nr_x + j, 1 ) = 0.9e6 + ( i-1 )*0.25e6;
+        array( (i-1)*nr_z + j, 2 ) = 0.6e6 + ( j-1 )*0.25e6;
+    end
 end
+
+% nr_x = 2;
+% nr_z = 1;
+% array = zeros(nr_x*nr_z,2);
+% for i = 1:nr_x
+%         array( i, 1 ) = Lx/2 + (-1)^i * 0.15*Lx;
+%         % array( i, 1 ) = Lx/2 + (-1)^i * 0.08*Lx - Lx/5;
+%         array( i, 2 ) = Lz/2;
+% end
 
 
 
@@ -59,6 +62,18 @@ if( strcmp(make_plots,'yes') )
 end
 
 
+% start matlabpool
+if(strcmp(mode,'cluster'))
+    cluster = parcluster('EulerLSF');
+    % cluster = parcluster('BrutusLSF');
+    jobid = getenv('LSB_JOBID');
+    mkdir(jobid);
+    cluster.JobStorageLocation = jobid;
+    cluster.SubmitArguments = '-W 8:00 -R "rusage[mem=3072]"';
+    matlabpool(cluster,16)
+end
+
+
 % calculate correlations
 n_ref = size(ref_stat,1);
 n_rec = size(array,1)-1;
@@ -67,24 +82,19 @@ c_it = zeros(n_ref,n_rec,length(t));
 
 fprintf('\n')
 flip_sr = 'no';
-for i = 1:n_ref
+parfor i = 1:n_ref
     
-    % if( strcmp(verbose,'yes') )
+    if( strcmp(verbose,'yes') )
         fprintf('reference station: %i\n',i)
-    % end
+    end
     
     % each reference station will act as a source once
     src = ref_stat(i,:);
     rec = array( find(~ismember(array,src,'rows') ) , :);
     
     % calculate the correlation for each pair
-    if( strcmp(mode,'slow') )
-%         [~,~] = run_forward('forward_green',src,rec,i,flip_sr);
-        [c_it(i,:,:),~] = run_forward('correlation',src,rec,i,flip_sr);
-    elseif(strcmp(mode,'fast'))
-        [G_2] = run_forward_green_fast_mex(mu, src);
-        [c_it(i,:,:)] = run_forward_correlation_fast_mex(G_2, source_dist, mu, rec);
-    end
+    [~,~] = run_forward('forward_green',src,rec,i,flip_sr);
+    [c_it(i,:,:),~] = run_forward('correlation',src,rec,i,flip_sr);
     
 end
 
@@ -108,5 +118,13 @@ end
 save( sprintf('../output/interferometry/array_%i_ref_big_test1.mat',n_ref), 'array', 'ref_stat')
 save( sprintf('../output/interferometry/data_%i_ref_big_test1.mat',n_ref), 'c_data', 't')
 
-toc
+
+% close matlabpool
+if(strcmp(mode,'cluster'))
+    matlabpool close
+end
+
+
+% clean up
+rmpath(genpath('../'))
 
