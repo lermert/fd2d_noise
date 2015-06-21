3cf00f55e23bd4e16c5ccf5d9cb6684eabfd3e9b
sagerk <korbinian.sager@erdw.ethz.ch>
2015-06-21 19:08:51 +0200

    update initial models for local inversion

--- inversion/start_inversion_local.m
+++ inversion/start_inversion_local.m
@@ -4,10 +4,14 @@
 % initial model
 x0 = ones(nx*nz, 1);
 
-% run inversion
-% x = LBFGS(x0,'get_obj_grad',0.1,5);
-% x = 4.8e10 * ( 1 + LBFGS(x0,'get_obj_grad',0.1,5) );
+% run source inversion
+x0 = ones(nx*nz, 1);
 x = steepest_descent(x0,'get_obj_grad',0.05,0);
+
+% run structure inversion
+% x0 = zeros(nx*nz, 1);
+% x = 4.8e10 * ( 1 + LBFGS(x0,'get_obj_grad',0.05,5) );
+% x = 4.8e10 * ( 1 + steepest_descent(x0,'get_obj_grad',0.05,0) );
 
 % save solution
 save ../output/solution.mat x
