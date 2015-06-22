8d0006ca7dd46b224335e7227c3e1838fcb0650c
sagerk <korbinian.sager@erdw.ethz.ch>
2015-06-22 11:23:14 +0200

    include correlation to result

--- inversion/start_inversion.m
+++ inversion/start_inversion.m
@@ -9,6 +9,7 @@ if(strcmp(mode,'cluster'))
     
     cluster = parcluster('EulerLSF8h');
     % cluster = parcluster('BrutusLSF8h');
+        
     jobid = getenv('LSB_JOBID');
     mkdir(jobid);
     cluster.JobStorageLocation = jobid;
@@ -22,15 +23,15 @@ end
 
 
 % run source inversion
-x0 = ones(nx*nz, 1);
-[x,c_final] = steepest_descent(x0,'get_obj_grad',0.5,0);
+% x0 = ones(nx*nz, 1);
+% [x,c_final] = steepest_descent(x0,'get_obj_grad',0.05,0);
 
 
 % run structure inversion
-% x0 = zeros(nx*nz, 1);
-% [x,c_final] = 4.8e10 * ( 1 + LBFGS(x0,'get_obj_grad',0.05,5) );
-% [x,c_final] = 4.8e10 * ( 1 + steepest_descent(x0,'get_obj_grad',0.05,0) );
-
+x0 = zeros(nx*nz, 1);
+% [x] = LBFGS(x0,'get_obj_grad',0.05,5);
+[x,c_final] = steepest_descent(x0,'get_obj_grad',0.05,0);
+x = 4.8e10 * ( 1 + x );
 
 % save solution
 save ../output/solution.mat x c_final
