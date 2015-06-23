360fa2d39e2505d215ae34dde05a1ce69e43b0f8
sagerk <korbinian.sager@erdw.ethz.ch>
2015-06-23 18:09:13 +0200

    test new model

--- inversion/start_inversion.m
+++ inversion/start_inversion.m
@@ -7,8 +7,8 @@ mode = 'cluster';
 if(strcmp(mode,'cluster'))
     addpath(genpath('../'))
     
-    cluster = parcluster('EulerLSF8h');
-    % cluster = parcluster('BrutusLSF8h');
+    % cluster = parcluster('EulerLSF8h');
+    cluster = parcluster('BrutusLSF8h');
         
     jobid = getenv('LSB_JOBID');
     mkdir(jobid);
@@ -27,11 +27,18 @@ end
 % [x,c_final] = steepest_descent(x0,'get_obj_grad',0.05,0);
 
 
+% run source inversion with lower and upper bounds
+x0 = ones(nx*nz, 1);
+xl = zeros(nx*nz, 1);
+xu = inf(nx*nz, 1);
+[x,c_final] = projected_steepest_descent(x0,xl,xu,'get_obj_grad',0.05,0);
+
+
 % run structure inversion
-x0 = zeros(nx*nz, 1);
+% x0 = zeros(nx*nz, 1);
 % [x] = LBFGS(x0,'get_obj_grad',0.05,5);
-[x,c_final] = steepest_descent(x0,'get_obj_grad',0.05,0);
-x = 4.8e10 * ( 1 + x );
+% [x,c_final] = steepest_descent(x0,'get_obj_grad',0.05,0);
+% x = 4.8e10 * ( 1 + x );
 
 % save solution
 save ../output/solution.mat x c_final
